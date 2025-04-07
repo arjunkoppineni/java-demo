bdc35d8a31790cb636cb5e382517ba70705ac898
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-04-07 10:54:26 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello Humans, Welcome to");
+        System.out.println("Hello Humans, Welcome");
     }
 }
